bd96ce9219e55e535d2f58a653cd7bc3f8cae1cf
SajjadAliZaidi <sajjadalizaidi00@gmail.com>
2021-08-05 10:43:30 +0500

    created extension structure with dummy react component
    Dummy Component is now being rendered on the popup.html page

---
+++ src/Dummy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Dummy = () => {
+  return (
+    <React.Fragment>
+      <h1>
+        Hello from Dummy
+      </h1>
+    </React.Fragment>
+  );
+}
+
+export default Dummy;
---
+++ src/popup.js
@@ -0,0 +1,5 @@
+import Dummy from './Dummy';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<Dummy />, document.getElementById('app'));
